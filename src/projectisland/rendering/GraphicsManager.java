724e4bb5cc8f07461c15b804f79901755f0e26e1
Kna <Kna@Kna-Laptop>
2013-05-09 01:30:26 +0200

    Graphics Updated

--- src/projectisland/rendering/GraphicsManager.java
+++ src/projectisland/rendering/GraphicsManager.java
@@ -7,6 +7,24 @@ public class GraphicsManager
 {
 	private static LinkedList<DrawableGameComponent> children = new LinkedList<DrawableGameComponent> ();
 	
+	public static void addChild(DrawableGameComponent c)
+	{
+		children.add(c);
+	}
+	
+	public static boolean removeChild(DrawableGameComponent c)
+	{
+		try
+		{
+			children.remove(c);
+			return true;
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+			return false;
+		}
+	}
+	
 	public static void draw(Graphics g)
 	{
 		for(DrawableGameComponent c : children)

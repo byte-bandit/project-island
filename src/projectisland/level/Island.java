894452b4f2204060ae132a756344d6bc4f1bf7c4
Stephan Pfistner <technik@pageformant.de>
2013-05-09 00:31:54 +0200

    basic tiles and config

---
+++ src/projectisland/level/Island.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectisland.level;
+
+/**
+ *
+ * @author subzeros
+ */
+public class Island {
+    private Tile[][] level = null;
+    
+    public Island() {
+        //level = generateLevel();
+    }
+    
+    /*public static Tile[][] generateLevel()
+    {
+        return new Tile[10];
+    }*/
+}
